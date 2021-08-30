43d3d6af42c8b6c2c9667dc60424017b96862cb7
Andrej <zarke998@gmail.com>
2021-08-30 12:41:11 +0200

    refactor: move team list service to own folder

--- src/app/team-list/team-list.component.ts
+++ src/app/team-list/team-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TeamService } from '../services/team.service';
+import { TeamService } from '../services/team-list-service/team.service';
 
 @Component({
   selector: 'app-team-list',
