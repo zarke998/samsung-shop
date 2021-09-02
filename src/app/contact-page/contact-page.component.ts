4d4773c231d6b1521d3203870293cdafea3aa69c
Andrej <zarke998@gmail.com>
2021-09-02 13:55:46 +0200

    feat: add contact form validation

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-page',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactPageComponent implements OnInit {
 
-  constructor() { }
+  contactForm : FormGroup = new FormGroup({});
 
-  ngOnInit(): void {
+  constructor(){
+
   }
 
+  ngOnInit(): void {
+    this.contactForm = new FormGroup({
+      name : new FormControl(null, Validators.required),
+      email : new FormControl(null, [Validators.required, Validators.email]),
+      subject : new FormControl(null, Validators.required),
+      message : new FormControl(null, [Validators.required, this.validateMessageLength])
+    });
+  }
+
+  get email(){
+    return this.contactForm.get("email");
+  }
+
+  get subject(){
+    return this.contactForm.get("subject");
+  }
+
+  get message(){
+    return this.contactForm.get("message");
+  }
+
+  validateMessageLength(control : FormControl){
+  if(control.value != null && control.value.length > 100){
+    console.log(control.value);
+    return {
+      'messageTooLong' : true
+    }
+  }
+  return null;
+}
+
+  contactSubmit(){
+    if(this.contactForm.valid){
+      alert("Thank you for contacting us! We'll reach out to you as soon as possible");
+    }
+    else{
+      alert("Please fill in all fields.");
+    }
+  }
 }
